e761b25211c9c20bd32d1bd07a33ef3f9062b2a8
Hind_Jammar <121793980+hindjammar@users.noreply.github.com>
2024-12-22 00:05:44 +0100

    added admin files

---
+++ admin/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  return (
+    <div >
+      <BrowserRouter> 
+    
+      </BrowserRouter>
+    
+    </div>
+  );
+}
+
+export default App;
